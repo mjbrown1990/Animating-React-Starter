5ff77b7f73474738ad8493ceb3466dfdd59e0d72
James Brown <james.brown@webmate.me>
2020-05-15 13:20:09 +0100

    L9

--- src/Toggle.js
+++ src/Toggle.js
@@ -8,13 +8,16 @@ const Toggle = () => {
         // opacity: isToggled ? 1 : 0,
         color: isToggled ? '#000' : 'green',
         // fontSize: isToggled ? '2em' : '5em',
-        y: isToggled ? 0 : -50
+        y: isToggled ? 0 : 1
     });
 
     return (
         <div>
             <animated.h1 style={{
-                transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+                transform: y.interpolate({
+                    range: [0, .25, .50, .75, 1],
+                    output: [0, -25, -50, -100, -50 ]
+                }).interpolate(y => `translate3d(0, ${y}px, 0)`),
                 color
             }}>
                 Hello
