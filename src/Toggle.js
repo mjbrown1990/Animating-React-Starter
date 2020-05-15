bc4fe223450b8eff06283d4c96cf3973768194b6
James Brown <james.brown@webmate.me>
2020-05-15 14:29:05 +0100

    L11

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
-    const [isToggled, setToggle] = useState(false);
+    const [items, setItems] = useState([
+        {
+            letter: "J",
+            key: 1
+        },
+        {
+            letter: "A",
+            key: 2
+        },
+        {
+            letter: "M",
+            key: 3
+        },
+        {
+            letter: "E",
+            key: 4
+        },
+        {
+            letter: "S",
+            key: 5
+        }
+    ]);
 
-    const transition = useTransition( isToggled, null, {
+    const transition = useTransition( items, item => item.key , {
         from: { opacity: 0 },
         enter: { opacity: 1 },
         leave: { opacity: 0 },
@@ -13,10 +34,13 @@ const Toggle = () => {
     return (
         <div>
             {transition.map(({ item, key, props }) =>(
-                item && <animated.h1 key={key} style={props}>Hello</animated.h1>
+                <animated.h1 style={props} key={key}>{item.letter}</animated.h1>
             ))}
 
-            <button onClick={() => setToggle(!isToggled) }>Toggle</button>
+            <button onClick={() => setItems([{
+                letter: "J",
+                key: 1
+            }]) }>Toggle</button>
         </div>
     );
 }
