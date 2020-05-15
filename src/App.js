2625d708f17b36d257386985e3767bf55e0c8ae0
James Brown <james.brown@webmate.me>
2020-05-15 11:45:28 +0100

    L4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
+
+import Toggle from './Toggle';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +18,9 @@ const App = () => {
         <img src={logo} className="logo" alt="logo" />
         <button className="menu-button">Menu</button>
       </header>
+      <main>
+        <Toggle />
+      </main>
     </animated.div>
   );
 }
